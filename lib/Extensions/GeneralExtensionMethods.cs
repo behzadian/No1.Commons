02ffd073804376ded56b46b72853ba8cdd14e93b
Homayoun Behzadian <6245340+behzadian@users.noreply.github.com>
2026-07-02 09:40:22 +0200

    Improvement for method IsUseless (#14)
    Co-authored-by: Hank <eqpc@hbn.dev>

--- lib/Extensions/GeneralExtensionMethods.cs
+++ lib/Extensions/GeneralExtensionMethods.cs
@@ -8,6 +8,8 @@ public static class GeneralExtensionMethods
 
 	public static bool IsUsable([NotNullWhen(true)] this object? obj) => obj != null;
 
+	public static bool IsUseless([NotNullWhen(false)] this object? obj) => obj == null;
+
 	[return: NotNull]
 	public static T Otherwise<T>(this T? value, T replacement)
 	where T : notnull => value.HasValue() ? value : replacement;
