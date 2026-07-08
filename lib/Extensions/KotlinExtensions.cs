407271b825cc7be963f910fc52111072ff9d31dc
Hank <eqpc@hbn.dev>
2026-07-08 10:21:38 +0200

    ...

--- lib/Extensions/KotlinExtensions.cs
+++ lib/Extensions/KotlinExtensions.cs
@@ -16,10 +16,10 @@ public static class KotlinExtensions
 		return value;
 	}
 
-	public static Task<T> InitAsync<T>(this T value, Action<T> action)
+	public static async Task<T> InitAsync<T>(this T value, Func<T, Task> action)
 	where T : notnull {
 		ArgumentNullException.ThrowIfNull(action);
-		action(value);
-		return Task.FromResult(value);
+		await action(value).ConfigureAwait(true);
+		return value;
 	}
 }
