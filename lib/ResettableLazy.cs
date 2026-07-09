642560db0e99c4e8a66430206cbde3b4dadc1c96
Hank <eqpc@hbn.dev>
2026-07-09 13:59:17 +0200

    1.3.5

---
+++ lib/ResettableLazy.cs
@@ -0,0 +1,34 @@
+﻿namespace No1.Commons;
+
+using System;
+using System.Threading;
+
+public sealed class ResettableLazy<T>
+{
+	private readonly Func<T> factory;
+	private readonly Lock @lock = new();
+
+	private Lazy<T> lazy;
+
+	public ResettableLazy(Func<T> factory) {
+		this.factory = factory ?? throw new ArgumentNullException(nameof(factory));
+		this.lazy = this.CreateLazy();
+	}
+
+	public T Value => this.lazy.Value;
+
+	public bool IsValueCreated => this.lazy.IsValueCreated;
+
+	public void Reset() {
+		lock (this.@lock) {
+			this.lazy = this.CreateLazy();
+		}
+	}
+
+	private Lazy<T> CreateLazy() {
+		return new Lazy<T>(
+			this.factory,
+			LazyThreadSafetyMode.ExecutionAndPublication
+		);
+	}
+}
