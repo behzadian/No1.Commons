9dface7dff91cb59a95c42a7218d4ff37a5a793e
Homayoun Behzadian <6245340+behzadian@users.noreply.github.com>
2026-07-09 14:11:32 +0200

    ResetableLazy and ExceptionUtiliy added (#18)

---
+++ lib/Utility/ExpressionUtility.cs
@@ -0,0 +1,29 @@
+﻿using No1.Commons.Exceptions;
+using System.Linq.Expressions;
+
+namespace No1.Commons.Utility;
+
+public static class ExpressionUtility
+{
+	public static string GetPropertyPath<TEntity>(
+		Expression<Func<TEntity, object>> expression
+	) {
+		ArgumentNullException.ThrowIfNull(expression);
+		Expression body = expression.Body;
+
+		// Remove boxing conversion for value types
+		if (body is UnaryExpression unary &&
+			unary.NodeType == ExpressionType.Convert) {
+			body = unary.Operand;
+		}
+
+		var members = new Stack<string>();
+
+		while (body is MemberExpression memberExpression) {
+			members.Push(memberExpression.Member.Name);
+			body = NullExpressionException.Exec(() => memberExpression.Expression);
+		}
+
+		return string.Join(".", members);
+	}
+}
