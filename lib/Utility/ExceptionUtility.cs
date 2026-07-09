8c506b9da5c1cedb090b44bad36058ebf1c9dfd3
Hank <eqpc@hbn.dev>
2026-07-09 12:34:13 +0200

    1.3.4

--- lib/Utility/ExceptionUtility.cs
+++ lib/Utility/ExceptionUtility.cs
@@ -1,8 +1,8 @@
 namespace No1.Commons.Utility;
 
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Skip")]
 public static class ExceptionUtility
 {
-	[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Skip")]
 	public static T ExecExceptionless<T>(Func<T> func, T defaultValue)
 		where T : notnull {
 		ArgumentNullException.ThrowIfNull(func);
@@ -13,7 +13,6 @@ public static class ExceptionUtility
 		}
 	}
 
-	[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Skip")]
 	public static T TryCatch<T>(Func<T> func, Func<Exception, T> catcher)
 		where T : notnull {
 		ArgumentNullException.ThrowIfNull(func);
@@ -22,6 +21,18 @@ public static class ExceptionUtility
 			return func();
 		} catch (Exception ex) {
 			return catcher(ex);
+		}
+	}
+
+	public static T TryThrow<T>(Func<T> func, Action<Exception> catcher)
+		where T : notnull {
+		ArgumentNullException.ThrowIfNull(func);
+		ArgumentNullException.ThrowIfNull(catcher);
+		try {
+			return func();
+		} catch (Exception ex) {
+			catcher(ex);
+			throw new InvalidOperationException("Exception while executing TryThrow", ex);
 		}
 	}
 }
