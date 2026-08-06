238f0657f9b42a9ff6c016d6f956b6f0c8cd98bd
Homayoun Behzadian <6245340+behzadian@users.noreply.github.com>
2026-08-06 14:49:25 +0200

    Parse options added (#22)
    Co-authored-by: Hank <eqpc@hbn.dev>

--- lib/Utility/ParseUtility.cs
+++ lib/Utility/ParseUtility.cs
@@ -1,132 +1,26 @@
-﻿namespace No1.Commons.Utility;
+﻿using System.Net;
+
+namespace No1.Commons.Utility;
 
 public static class ParseUtility
 {
-	public static short? GetShort(this string value) {
-		if (short.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
+	public static T? ParseAs<T>(this string value) where T : struct, IParsable<T> {
+		return T.TryParse(value, null, out var parsed) ? parsed : null;
 	}
 
-	public static int? GetInt32(this string value) {
-		if (int.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
+	public static Uri? ParseAsUri(this string value) {
+		return Uri.TryCreate(value, UriKind.RelativeOrAbsolute, out var parsed) ? parsed : null;
 	}
 
-	public static long? GetLong(this string value) {
-		if (long.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
+	public static Version? ParseAsVersion(this string value) {
+		return Version.TryParse(value, out var parsed) ? parsed : null;
 	}
 
-	public static double? GetDouble(this string value) {
-		if (double.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
+	public static IPAddress? ParseAsIPAddress(this string value) {
+		return IPAddress.TryParse(value, out var parsed) ? parsed : null;
 	}
 
-	public static Int128? GetInt128(this string value) {
-		if (Int128.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
-	}
-
-	public static bool? GetBoolean(this string value) {
-		if (bool.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
-	}
-
-	public static char? GetChar(this string value) {
-		if (char.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
-	}
-
-	public static byte? GetByte(this string value) {
-		if (byte.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
-	}
-
-	public static sbyte? GetSignedByte(this string value) {
-		if (sbyte.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
-	}
-
-	public static decimal? GetDecimal(this string value) {
-		if (decimal.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
-	}
-
-	public static float? GetFloat(this string value) {
-		if (float.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
-	}
-
-	public static uint? GetUnsignedInt(this string value) {
-		if (uint.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
-	}
-
-	public static nint? GetNint(this string value) {
-		if (nint.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
-	}
-
-	public static nuint? GetNuint(this string value) {
-		if (nuint.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
-	}
-
-	public static ulong? GetUnsignedLong(this string value) {
-		if (ulong.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
-	}
-
-	public static ushort? GetUnsignedShort(this string value) {
-		if (ushort.TryParse(value, out var parsed)) {
-			return parsed;
-		} else {
-			return null;
-		}
+	public static IPEndPoint? ParseAsIPEndPoint(this string value) {
+		return IPEndPoint.TryParse(value, out var parsed) ? parsed : null;
 	}
 }
